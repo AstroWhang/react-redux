f2671d8f5285fc408ff370d75cf7354fbbed2944
Astro Whang <astro.whang@bluevolt.com>
2019-07-16 09:59:20 -0700

    removed whitespaces

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { increment, decrement, toggleTrue } from './action';
+
 
 function App() {
+
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.loggedReducer);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <br></br>
+      <br></br>
+      <button onClick={() => dispatch(toggleTrue())}>Log In</button>
+      {isLogged ? <h3>You're logged in!</h3> : <h3>You're NOT logged in!</h3>}
     </div>
   );
 }
